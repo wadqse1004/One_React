6aaaf6740ba95eaa427226008b36c8f3fefabaf8
박상준 <parksj@bagsangjuns-MacBook-Air.local>
2025-07-16 22:59:05 +0900

    추가 수정 기능추가

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,14 +2,50 @@ import "./App.css";
 import Header from './component/Header';
 import Editor from './component/Editor';
 import List from './component/List';
+import { useState, useRef } from 'react';
+
+const mockData = [
+  {
+    id : 0,
+    isDone : false,
+    content : "리엑트 공부하기" ,
+    date : new Date().getTime(),
+  },
+  {
+    id : 1,
+    isDone : false,
+    content : "빨래하기" ,
+    date : new Date().getTime(),
+  },
+  {
+    id : 2,
+    isDone : false,
+    content : "노래 연습하기" ,
+    date : new Date().getTime(),
+  },
+]
 
 function App() {
+  
+  const [todos, setTodos] = useState([mockData]);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id : idRef.current++,
+      isDone : false,
+      content : content,
+      date : new Date().getTime()
+    }
+
+    setTodos([newTodo, ...todos]);
+  }
 
   return (
     <div className='App'>
       <Header/>
-      <Editor/>
-      <List/>
+      <Editor onCreate={onCreate}/>
+      <List todos={todos}/>
     </div>
   )
 }
--- section08/src/component/List.jsx
+++ section08/src/component/List.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
 import "./List.css";
 import TodoItem from './TodoItem';
 
-const List = () => {
+const List = ( {todos} ) => {
+
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSea;rch(e.target.value);
+    }
+
+    const getFilteredData = () => {
+        if(search === "")
+        {
+            return todos;
+        }
+
+        return  todos.filter((todo) => 
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        );
+    }
+
+    const filteredTodos = getFilteredData();
+
     return (
     <div className='List'>
         <h4>Todo List 🌱</h4>
-        <input placeholder='검색어를 입력하세요.' />
+        <input value={search} onChange={onChangeSearch} placeholder='검색어를 입력하세요.' />
         <div className='Todos_wrapper'>
-            <TodoItem/>
-            <TodoItem/>
-            <TodoItem/>
+            {filteredTodos.map((todo) => {
+                return <TodoItem key={todo.id} {...todo}/>
+            })}
         </div>
     </div>
     )
