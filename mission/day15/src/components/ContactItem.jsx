cbbd6de07599bd9bb4dfc072f0c97c15042d7511
SangJun Park <wadqse1004@naver.com>
2025-07-22 02:25:29 +0900

    readme 수정

--- mission/day15/src/components/ContactItem.jsx
+++ mission/day15/src/components/ContactItem.jsx
@@ -1,11 +1,11 @@
-import "./ContactItem.css";
+  import "./ContactItem.css";
 
-export default function ContactItem({id, name, contact, onDeleteContact}) {
-  return (
-    <div className="ContactItem">
-      <div className="name">{name}</div>
-      <div className="contact">{contact}</div>
-      <button onClick={() => onDeleteContact(id)}>🗑️ Remove</button>
-    </div>
-  );
-}
+  export default function ContactItem({id, name, contact, onDeleteContact}) {
+    return (
+      <div className="ContactItem">
+        <div className="name">{name}</div>
+        <div className="contact">{contact}</div>
+        <button onClick={() => onDeleteContact(id)}>🗑️ Remove</button>
+      </div>
+    );
+  }
