e508395b165c42c552c7b1e45d5cb3b069d9a889
SangJun Park <wadqse1004@naver.com>
2025-07-27 23:53:29 +0900

    동적경로 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import New from './pages/New';
 import Diary from './pages/diary';
 import Notfound from './pages/Notfound';
 import Home from './pages/home';
+import Edit from './pages/Edit';
 
   function App() {
 
@@ -19,12 +20,14 @@ import Home from './pages/home';
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
+        <Link to={"/Edit"}>Edit</Link>
       </div>
       <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home/>}/> 
         <Route path="/new" element={<New/>}/> 
-        <Route path="/diary" element={<Diary/>}/> 
+        <Route path="/diary/:id" element={<Diary/>}/> 
+        <Route path="/Edit/:id" element={<Edit/>}/> 
         <Route path='*' element = {<Notfound/>}/>
       </Routes>
       </>
